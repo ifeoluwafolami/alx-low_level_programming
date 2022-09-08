f7e9543e9aa0ab060b25347eea80003731a87a86
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-08 15:07:35 -0700

    Betty, 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 
-/**
+/*
  * A program that prints 'Programming
  * is like building a multilingual puzzle',
  * followed by a new line.
  */
 
+/**
+ * Puts returns the string, exactly as is.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
