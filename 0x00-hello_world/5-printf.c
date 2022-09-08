74342691948b903a1847c3ace55fe6b582b16edc
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-08 15:18:32 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void main()
+{
+	char statement[] = "with proper grammar, but the outcome is a piece of art";
+	printf("%s\n", statement)
+}
