bf57dbb391e70811c3906438d2cf871aeea6c45e
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-08 14:40:52 -0700

    4-fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 
-/*A program that prints 'Programming is like building a multilingual puzzle', followed by a new line.*/
+/*A program that prints 'Programming 
+ * is like building a multilingual puzzle', 
+ * followed by a new line.*/
 
 int main(void)
 
 {
 	char statement[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(statement);
+
 	return 0;
 }
