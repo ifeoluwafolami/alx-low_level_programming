3bce12c88d64af1e1c2a886077389ad81b90ed19
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-08 15:04:34 -0700

    Betty fix for Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-/*
+/**
  * A program that prints 'Programming
  * is like building a multilingual puzzle',
  * followed by a new line.
  */
 
-int main(void) /*Entry point*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
