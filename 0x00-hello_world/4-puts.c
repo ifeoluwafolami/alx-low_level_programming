d74bd37efdad11c1dd9e699781471837c10ebcea
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-08 14:38:10 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/*A program that prints 'Programming is like building a multilingual puzzle', followed by a new line.*/
+
+int main(void)
+
+{
+	char statement[] = "\"Programming is like building a multilingual puzzle";
+	puts(statement);
+	return 0;
+}
