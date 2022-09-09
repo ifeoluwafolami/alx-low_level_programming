68ff4e73ab31c92dba525481c384de261e6c319e
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-09 16:56:31 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (
+					((i != j) && (i != k) && (j != k))
+				       	&& ((j > i) && (k > j))
+				)
+				{
+					putchar(nums[i] + '0');
+					putchar(nums[j] + '0');
+					putchar(nums[k] + '0');
+					if (!((nums[i] == 7) && (nums[j] == 8) && (nums[k] == 9)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
