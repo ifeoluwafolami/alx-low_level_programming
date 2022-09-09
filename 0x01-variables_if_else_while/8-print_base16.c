6e97789ef4f5899aced6d9bf00c7ce336b410633
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-09 15:39:24 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i;
+
+	char alps[] = "abcdef";
+	int j;
+
+	for (i = 0; i < 10; i++)
+		putchar(nums[i] + '0');
+
+	for (j = 0; j < 6; j++)
+		putchar(alps[j]);
+	putchar('\n');
+	return (0);
+}
