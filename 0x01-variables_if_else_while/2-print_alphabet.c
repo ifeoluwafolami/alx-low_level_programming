ae6db8ee77d3cc83fc5ca9cdb3aae7ad4d0cfb3b
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-09 13:59:49 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
+	
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(ch[i]);
+	}
+	putchar('\n');
+	return (0);
+}
