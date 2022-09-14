add1cfad626b801301194c388bc0e70cbb1763e7
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-13 21:15:55 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add -returns the sum of two integers
+ * @m: number being added
+ * @n: number being added
+ * Return: Value of sum
+ */
+int add(int m, int n)
+{
+	int sum;
+	sum = m + n;
+
+	return (sum);
+}
