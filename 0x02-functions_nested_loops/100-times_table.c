b4d4cb0bed9ac9abfcda0a3d4e44cc07251bd079
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-14 16:21:34 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,12 @@ void print_times_table(int n)
 {
 	int i, j, prod;
 
-	if ((n > 0) && (n < 15))
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+	else if ((n > 0) && (n < 15))
 	{
 		for (i = 0; i <= n; i++)
 		{
