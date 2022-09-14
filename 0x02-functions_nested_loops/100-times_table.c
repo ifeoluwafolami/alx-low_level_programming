a150155d9e4a70eabab9708344dc1a3dd0946c78
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-14 15:07:49 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,43 +3,43 @@
  * print_times_table - prints the time table of an integer n,
  * starting with 0
  * @n: integer being processed
- * Return: Always 0 (Success)
  */
 void print_times_table(int n)
 {
 	int i, j, prod;
 
-	if ((n < 0) || (n > 15))
-	{
-		;
-	}
-	else
+	if ((n > 0) && (n < 15))
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				prod = i * j;
-				if (prod >= 10)
+				if (prod < 10)
 				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod + '0');
+				}
+				else if ((prod >= 10) && (prod < 100))
+				{
+					_putchar(' ');
 					_putchar((prod / 10) + '0');
 					_putchar((prod % 10) + '0');
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
 				}
 				else
 				{
-					_putchar(prod + '0');
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
+					_putchar(((prod % 100) % 10) + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
+			_putchar('\n');
 		}
 	}
 }
