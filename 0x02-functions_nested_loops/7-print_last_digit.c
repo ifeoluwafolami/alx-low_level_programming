35cce166f7493ef3377867fa1a8d70c0d405177b
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-13 20:33:21 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number being analyzed
+ * Return: Value of last digit of n
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+		n = -n;
+	_putchar(n + '0');
+	return (n);
+}
