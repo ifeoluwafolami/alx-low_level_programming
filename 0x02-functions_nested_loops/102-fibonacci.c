7b26f4216acdf59d4889868fe672908d9116679c
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-14 17:58:57 -0700

    14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 1, j = 2, k;
+
+	for (k = 0; k < 50; k++)
+	{
+		if (k == 0)
+		{
+			printf("%i, ", i);
+		}
+		else if (k == 1)
+		{
+			printf("%i, ", j);
+		}
+		else
+		{
+			j += i;
+			i = j - i;
+			if (k != 49)
+			{
+				printf("%i, ", j);
+			}
+			else
+			{
+				printf("%i\n", j);
+			}
+		}
+	}
+	return (0);
+}
