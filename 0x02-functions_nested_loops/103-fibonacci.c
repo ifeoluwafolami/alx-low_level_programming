b274be5ef5aa139fd3950171693d7f6582fb624c
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-14 18:18:36 -0700

    15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of the even-valued terms of the Fibonacci sequence
+ * below 4,000,000.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long i = 1, j = 2;
+
+	while (j < 4000000)
+	{
+		j += i;
+		i = j - i;	
+		if ((j % 2) == 0)
+		{
+			if ((j + i) < 4000000)
+			{
+				printf("%ld, ", j);
+			}
+			else
+			{
+				printf("%ld\n", j);
+			}
+		}
+	}
+	return (0);
+}
