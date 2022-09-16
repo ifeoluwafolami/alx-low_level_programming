621744b1b99a1b3f98272021e306ab7c0aecd7bd
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-15 21:30:50 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 1 to 100; followed by a new line.
+ * Multiples of 3 are replaced by 'Fizz',
+ * Multiples of 5 are replaced by 'Buzz',
+ * Multiples of both 3 and 5 are replaced by 'FizzBuzz'.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			if (i % 3 == 0)
+				printf("Fizz ");
+			else
+			{
+				if (i % 5 == 0)
+				{
+					printf("Buzz ");
+				}
+				else
+					printf("%i ", i);
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
