39c83b49fa77ed4e88c4bcf3035121f3d03681a4
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-09-20 11:20:04 -0700

    Task 7, redone

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,13 +5,22 @@
  */
 void puts_half(char *str)
 {
-	int i, j = 0, half;
+	int i, j = 1, half;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		j++;
 	}
-	half = j / 2;
+	if (j % 2 != 0)
+	{
+		half = (j - 1) / 2;
+	}
+	else
+	{
+		half = j / 2;
+	}
+
+
 	for (; str[half] != '\0'; half++)
 	{
 		_putchar(str[half]);
