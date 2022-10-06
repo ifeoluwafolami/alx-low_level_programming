973c24d885683aef33684827b7396cb437b59c7c
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-06 05:32:50 -0700

    Yask 2, redo

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strchr - locates a character in a string
  * @s: string
@@ -17,5 +18,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
-	return (0);
+	return (NULL);
 }
