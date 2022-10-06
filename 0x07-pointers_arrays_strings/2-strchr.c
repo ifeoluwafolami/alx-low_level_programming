5bfd5dd1a79ae7ff9b5f51b61669e88450377df6
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-06 05:45:46 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for (; *s != '\0'; s++)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (s + i);
+			return (s);
 		}
-	}
-	if (s[i] == c)
-	{
-		return (s);
 	}
 	return (NULL);
 }
