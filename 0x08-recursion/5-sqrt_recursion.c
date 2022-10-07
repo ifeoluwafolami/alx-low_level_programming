42afbd47952fad7cbdc72a751d583a15b1177508
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-07 08:34:58 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: -1 if n does not have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,8 @@ void _print_rev_recursion(char *s);
 
 int _strlen_recursion(char *s);
 
+int _sqrt(int n, int i);
+
 int factorial(int n);
 
 int _pow_recursion(int x, int y);
