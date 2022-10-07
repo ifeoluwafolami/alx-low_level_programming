bac6a24cb043077d0b7b7f33d5f2d8fb67625960
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-07 01:37:11 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string being scanned
+ * @accept: string whose characters are to be matched
+ * Return: NULL or pointer to the byte in s that matched
+ * one of the bytles in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned i, j;
+	
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+	return (NULL);
+}
