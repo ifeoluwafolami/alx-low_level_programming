9227c9769e44f9e9a70b0cb312084b046caa91a5
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-16 15:59:45 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,28 +8,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *c;
-	unsigned int i, j, k, l;
+        char *c;
+        unsigned int i, j, k, l;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	c = malloc(sizeof(char) * (i + j + 1));
-	if (c == NULL)
-	{
-		free(c);
-		return (NULL);
-	}
-	for (k = 0; k <= i; k++)
-		c[k] = s1[k];
-	for (l = 0; l <= j; k++, l++)
-	{
-		c[k] = s2[l];
-	}
-	return (c);
+        if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
+        for (i = 0; s1[i] != '\0'; i++)
+                ;
+        for (j = 0; s2[j] != '\0'; j++)
+                ;
+        c = malloc(sizeof(char) * (i + j + 1));
+        if (c == NULL)
+        {
+                free(c);
+                return (NULL);
+        }
+        for (k = 0; k < i; k++)
+                c[k] = s1[k];
+        for (l = 0; l <= j; k++, l++)
+        {
+                c[k] = s2[l];
+        }
+        return (c);
 }
