e509ae6467b822dfe9383b1f1af807e85036cdc1
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-16 13:18:11 -0700

    Task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size ==  0)
 		return (NULL);
 	ch = malloc(size * sizeof(c));
-	if (cr == NULL)
+	if (ch == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		ch[i] = c;
