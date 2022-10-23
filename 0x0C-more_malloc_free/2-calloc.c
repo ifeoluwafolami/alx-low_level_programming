af68841be3cf7eaaf3ae20fb0a3e3f690a81e5bf
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-23 03:49:23 -0700

    Checker test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,22 +1,31 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 /**
- * _calloc - allocates memory for an array using malloc
- * @nmemb: number of members
- * @size: size of element
- * Return: pointer to memory
- */
+  * _calloc - allocates memory of an array using malloc.
+  * @nmemb: number of elements in array.
+  * @size: size of elements of array.
+  *
+  * Return: NULL is size or nmemb == 0.
+  * NULL if malloc fails.
+  * Pointer to memory allocated if successful.
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *p;
 	unsigned int i;
-	void *ch;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ch = malloc(nmemb * size);
-	if (ch == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		*((char *)ch + i) = 0;
-	return (ch);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+
+	return (p);
 }
