fe3b6d98d5a9e2244ac4796aa783150634a42258
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-28 13:57:08 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - prints a name
  * @name: the name
@@ -6,6 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)	
+	if (f != NULL)
 		f(name);
 }
