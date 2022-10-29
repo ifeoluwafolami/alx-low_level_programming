2e2af2933626ded0d6697417b84dc4c4006162e4
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-10-28 22:56:09 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != NULL && size != 0)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	if (action == NULL && size == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
