2b52d304904caa4a31ef220e0982344fa7c4b0f9
Ifeoluwa Folami <folamihephzibah@gmail.com>
2022-11-04 10:38:34 -0700

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,10 +19,11 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return ((ops[i]).f);
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
