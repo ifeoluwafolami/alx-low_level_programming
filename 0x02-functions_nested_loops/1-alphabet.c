08e1a608e078d96c359e343bd91fa002fcec309e
Ifeoluwa Folami <folamihephzibah@gmail.com>
2023-03-21 21:47:06 -0700

    Tasks, updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,4 +4,10 @@
  */
 void print_alphabet(void)
 {
+	char alph_low[] = 'abcdefghijklmnopqrstuvwxyz';
+	int i;
 
+	for (i = 0; alph_low[i] != '\0'; i++)
+		_putchar(alph_low[i]);
+	_putchar("\n");
+}
