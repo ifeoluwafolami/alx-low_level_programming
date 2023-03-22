7641f60345a664b3457784c4052ce65896ba8c21
Ifeoluwa Folami <folamihephzibah@gmail.com>
2023-03-21 22:24:35 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number
+ * Return: 1 and + if positive, 0 if zero, -1 and - if negative
+ */
+int print_sign(int n)
+{
+	
