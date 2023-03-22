5d72056a3f992794a7e82edbf475fdc725c5a02f
Ifeoluwa Folami <folamihephzibah@gmail.com>
2023-03-21 21:55:02 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,9 @@ void print_alphabet_x10(void)
 	for (j = 0; j != 10; j++)
 	{
 		for (i = 0; alph_low[i] != '\0'; i++)
+		{
 			_putchar(alph_low[i]);
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
