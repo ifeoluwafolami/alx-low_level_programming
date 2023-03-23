b5c0f3e8bf020ebe5c1cefd4c4555b16ded7cbf8
Ifeoluwa Folami <folamihephzibah@gmail.com>
2023-03-23 09:35:16 -0700

    task 8, fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,10 +7,14 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
+	if (n > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	else
 		_putchar('\n');
-	}
+
 }
