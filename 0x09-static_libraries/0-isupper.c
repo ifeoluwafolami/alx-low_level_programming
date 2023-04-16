d476435dc24b24ba4dc430aa0cfbaf045484150a
Ifeoluwa Folami <folamihephzibah@gmail.com>
2023-04-16 16:24:53 -0700

    Tasks, updated 11

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * _isupper - checks for uppercase
- * @c: character
- * Return: 1 if uppercase, 0 otherwise
+ * _isupper - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
  */
+
+
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
-	else
-		return (0);
+
+	return (c >= 65 && c <= 90);
 }
